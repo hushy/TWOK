c0cbe33946c31a11a495ac3255a8b5380e9d62a0
lucas.verdonk <lucas.verdonk@datadoghq.com>
2024-02-25 15:05:53 +0100

    gitignore simplify loop

--- src/game.js
+++ src/game.js
@@ -1,24 +1,15 @@
-const readline = require('readline').createInterface({
-    input: process.stdin,
-    output: process.stdout
-  });
+const readline = require('readline-sync')
   
-  const main = () => {
-    console.log("Welcome to the Adventure Game!");
-    readline.question("Do you go left or right? ", answer => {
-      answer = answer.trim()
-      if (answer === "left") {
-        console.log("You've encountered a friendly dragon!");
-      } else if (answer === "right") {
-        console.log("You've stumbled into a trap!");
-      } else {
-        console.log("Invalid choice. Game over.");
-      }
-      readline.close();
-    });
-  };
-  
-  console.log("test!");
+console.log("Welcome to the Adventure Game!");
 
-  main();
-  
+
+// GAME LOOP
+while (true) {
+
+  var yn = readline.question(" ? ");
+  if(yn === 'REPONSE') {
+    console.log("Hooray!");
+  } 
+
+
+}
