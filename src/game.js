24be3a6fdf9d25fc7e799b16c0059ce4c292ff22
lverdonk <lverdonk@gmail.com>
2024-02-25 16:01:48 +0100

    qscii

--- src/game.js
+++ src/game.js
@@ -1,7 +1,18 @@
 const readline = require('readline-sync')
-  
+const fs = require('fs');
+const path = require('path');
+
 console.log("Welcome to the Adventure Game!");
 
+// Function to display ASCII art
+function display_art(name) {
+  try {
+    const asciiArt = fs.readFileSync('./art/'+name, 'utf8');
+    console.log(asciiArt);
+  } catch (error) {
+    console.error("Could not read ASCII art file:", error.message);
+  }
+}
 
 // GAME LOOP
 while (true) {
@@ -9,6 +20,7 @@ while (true) {
   var yn = readline.question(" ? ");
   if(yn === 'REPONSE') {
     console.log("Hooray!");
+    display_art('castle')
   } 
 
 
