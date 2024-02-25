24b34c76c9b489990205b6f6d38b5fa1e7b55e89
lucas.verdonk <lucas.verdonk@datadoghq.com>
2024-02-25 17:45:47 +0100

    events

--- src/game.js
+++ src/game.js
@@ -13,48 +13,62 @@ function display_art(name) {
   }
 }
 
+function player_choice(){
+  return readline.question("What do you want to do?")
+}
 
-
-var name = readline.question(" what is your name ? ")
-console.log("Welcome to the Adventure Game "+name);
-display_art("castle")
-console.log("You arrive in front of a castle what do you do ?\n" +
-    "1. Use the front door.\n" +
-    "2. Climb the wall to the tower window\n" +
-    "3. Go swim in the moar." )
-
-var choice = readline.question("?")
-
-if (choice == "1") {
+function dragon_lair(){
   display_art("Dragon")
   console.log("You have entered th dragons lair and awoken him. \n" +
       "1. Flee.\n" +
       "2.Fight \n" +
       "3.Hide ." )
+      var choice = player_choice()
+
 }
 
-if (choice == "2") {
+function fake_princess_chamber(){
   display_art("princess")
   console.log("You climb the tower and find your self nose to nose with a princess.\n There" +
       "is a door.\n" +
       "1. kill her.\n" +
-      "2.get back down. \n" +
-      "3.take the pricess down ." )
+      "2. get back down. \n" +
+      "3. take the pricess down ." )
+      var choice = player_choice()
+
 }
 
-if (choice == "3") {
+function sewer_entrance(){
   display_art("croco")
-  console.log("You see an opening in the sewer grate guarded by a huge crocodile \n." +
+    console.log("You see an opening in the sewer grate guarded by a huge crocodile \n." +
+        "1. fight .\n" +
+        "2. sneek. \n" +
+        "3. go back." )
+        var choice = player_choice()
 
-      "1.fight .\n" +
-      "2.sneek. \n" +
-      "3.go back." )
 }
-var gl=0
 
-// GAME LOOP
-while (true) {
-//.log("gameloop"+gl)
-//gl=gl+1
+function castle_entrance() {
+  display_art("castle")
+  console.log("You arrive in front of a castle what do you do ?\n" +
+      "1. Use the front door.\n" +
+      "2. Climb the wall to the tower window\n" +
+      "3. Go swim in the moar." )
+  
+      var choice = player_choice()
+  
+  switch (choice ) {
+    case '1':
+      return dragon_lair()
+    case '2':
+      return fake_princess_chamber()
+    case '3':
+      return sewer_entrance()
+  }
+
 }
-// end of GAME LOOP
+
+var name = readline.question(" what is your name ? ")
+console.log("Welcome to the Adventure Game "+name);
+
+castle_entrance()
