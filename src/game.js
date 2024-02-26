8a25aac815d5b818cf753d313ce70bda7ae34014
lverdonk <lverdonk@gmail.com>
2024-02-26 10:47:28 +0100

    EVENTS

--- src/game.js
+++ src/game.js
@@ -2,7 +2,22 @@ const readline = require('readline-sync')
 const fs = require('fs');
 const path = require('path');
 
+const monsters = {
+  killer_princess:
+      {hp:20,atk:2},
+  dragon:
+      {hp:200,atk:20},
+  crocodile:
+      {hp:10,atk:1}
+}
+var player = {
+  hp:20,
+  atk:3
+}
 
+function game_over() {
+  console.log("game over");
+}
 // Function to display ASCII art
 function display_art(name) {
   try {
@@ -12,18 +27,55 @@ function display_art(name) {
     console.error("Could not read ASCII art file:", error.message);
   }
 }
-
 function player_choice(){
   return readline.question("What do you want to do?")
+}
+function fight(monster, victory, defeat){
+
+
+
+  while (player.hp > 0 && monster.hp > 0){
+    console.log("hp hero : " + player.hp)
+    console.log("monster hp : " + monster.hp)
+
+    console.log("1. ATTACK 2. DEFEND")
+    var choice=   player_choice()
+    var atk = player.atk
+    var monster_atk = monster.atk
+    if (choice == "1") {
+
+    }
+    if (choice == "2") {
+      monster_atk = monster_atk/2
+      atk = 0
+    }
+    monster.hp=monster.hp-atk
+    player.hp=player.hp-monster_atk
+  }
+
+  if (monster.hp<=0){
+    return victory()
+  }else{
+    return defeat()
+  }
+
 }
 
 function dragon_lair(){
   display_art("Dragon")
   console.log("You have entered th dragons lair and awoken him. \n" +
-      "1. Flee.\n" +
+      "1.Flee.\n" +
       "2.Fight \n" +
       "3.Hide ." )
       var choice = player_choice()
+  switch (choice ) {
+    case '1':
+      return castle_entrance()
+    case '2':
+      return fight(monsters.dragon,castle_entrance,game_over)
+    case '3':
+      return
+  }
 
 }
 
@@ -31,21 +83,35 @@ function fake_princess_chamber(){
   display_art("princess")
   console.log("You climb the tower and find your self nose to nose with a princess.\n There" +
       "is a door.\n" +
-      "1. kill her.\n" +
-      "2. get back down. \n" +
-      "3. take the pricess down ." )
+      "1. Kill her.\n" +
+      "2. Get back down. \n" +
+      "3. Take the pricess down ." )
       var choice = player_choice()
-
+  switch (choice ) {
+    case '1':
+      return
+    case '2':
+      return castle_entrance()
+    case '3':
+      return
+  }
 }
 
 function sewer_entrance(){
   display_art("croco")
     console.log("You see an opening in the sewer grate guarded by a huge crocodile \n." +
-        "1. fight .\n" +
-        "2. sneek. \n" +
-        "3. go back." )
+        "1. Fight .\n" +
+        "2. Sneek. \n" +
+        "3. Go back." )
         var choice = player_choice()
-
+  switch (choice ) {
+    case '1':
+      return
+    case '2':
+      return
+    case '3':
+      return castle_entrance()
+  }
 }
 
 function castle_entrance() {
